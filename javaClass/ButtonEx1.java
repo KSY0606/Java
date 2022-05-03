d9800626870243c65fb63ecd3a51e22f86c06722
KSY0606 <tkddbs9506@naver.com>
2022-05-03 15:02:46 +0900

    중첩 인터페이스

---
+++ javaClass/ButtonEx1.java
@@ -0,0 +1,14 @@
+package javaClass;
+
+public class ButtonEx1 {
+
+	public static void main(String[] args) {
+
+		Button button = new Button();
+		button.setOnClickListener(new CallListener());
+		button.touch();
+		button.setOnClickListener(new MessageListener());
+		button.touch();
+	}
+
+}
---
+++ javaClass/NClass.java
@@ -0,0 +1,10 @@
+package javaClass;
+// 바깥 클래스
+public class NClass {
+	// 내부 인터페이스
+	interface intre {
+		//추상 메서드
+		void iMethod();
+		
+	}
+}
