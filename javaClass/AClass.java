bfbd643a906d18d4066038f337b80a2d7468c690
KSY0606 <tkddbs9506@naver.com>
2022-05-03 14:56:35 +0900

    중첩 클래스

--- javaClass/AClass.java
+++ javaClass/AClass.java
@@ -5,7 +5,7 @@ public class AClass {
 	class BClass {
 		//필드
 		int bField;
-		// static int bField2; 정적필드 불가능
+		// static int bField2; 인스턴스 클래스에서는 정적필드 불가능
 		//생성자
 		public BClass() {
 			
@@ -30,4 +30,19 @@ public class AClass {
 			
 		}
 	}
+	void aMethod() {
+		class DClass {
+			//필드
+			int dField;
+			//static int dField2;
+			//생성자
+			public DClass() {
+				System.out.println("DClass가 생성됨");
+			}
+			//메서드
+			void dMethod() {
+			}
+			//	static void dMethod2() {}
+		}
+	} // aMethod()
 }
