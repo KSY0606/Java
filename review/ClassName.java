1614a45db01246ee64389b824a646d63268c1dd1
KSY0606 <tkddbs9506@naver.com>
2022-05-07 20:28:54 +0900

    클래스

---
+++ review/ClassName.java
@@ -0,0 +1,15 @@
+package review;
+
+public class ClassName {
+
+	int method1(int x, int y) {
+		int result = x + y;
+		return result;
+	}
+	
+	void method2() {
+		int result1 = method1(10, 20);
+		double result2 = method1(10,20);
+		
+	}
+}
